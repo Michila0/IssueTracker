10d5d29e27f77f0118ece27bec0b629f08df0adc
Michila0 <michila.bimsara12@gmail.com>
2026-05-21 14:52:43 +0530

    feat: update the web resposive

--- issue-tracker-client/src/components/IssueCard.tsx
+++ issue-tracker-client/src/components/IssueCard.tsx
@@ -111,7 +111,7 @@ export const IssueCard: React.FC<IssueCardProps> = ({ issue, onEdit, onDelete })
           </div>
 
           {/* Action buttons */}
-          <div className="flex items-center space-x-2 opacity-0 group-hover:opacity-100 focus-within:opacity-100 transition-opacity duration-300">
+          <div className="flex items-center space-x-2 opacity-100 md:opacity-0 md:group-hover:opacity-100 md:focus-within:opacity-100 transition-opacity duration-300">
             {/* Edit Button */}
             <button
               onClick={() => onEdit(issue)}
